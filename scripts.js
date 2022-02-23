89a2258f1a5c7cd5f8fb4f729a559eb2ad877570
andrer54 <andrer54@gmail.com>
2022-02-23 08:39:38 -0300

    Working on localStorage

--- scripts.js
+++ scripts.js
@@ -16,6 +16,16 @@ const Modal = {
     }
 }
 
+const Storage = {
+    get(){
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+
+    set(transactions){
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
+
 const transactions = [
     {
         id: 1,
@@ -39,7 +49,7 @@ const transactions = [
 ]
 
 const Transaction = {
-    all: transactions,
+    all: Storage.get(),
     add(transaction){
         Transaction.all.push(transaction)
 
@@ -81,12 +91,12 @@ const DOM = {
     addTransaction(transaction, index){
         
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
-        
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction){
+    innerHTMLTransaction(transaction, index){
 
         const CSSclasses = transaction.amount > 0 ? "income" : "expense";
         const amount = Utils.formatCurrency(transaction.amount);
@@ -96,7 +106,7 @@ const DOM = {
             <td class="${CSSclasses}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         `
        
@@ -214,14 +224,16 @@ const Form = {
 }
 
 
+
 const App = {
     init(){
 
-        Transaction.all.forEach(function(transaction){
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach(function(transaction, index){
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance();
+        Storage.set(Transaction.all)
     },
     reload () {
         DOM.clearTransactions();
@@ -231,9 +243,3 @@ const App = {
 
 App.init();
 
-Transaction.add({
-    id: 49,
-    description: "transação adicionada",
-    amount: -350,
-    date: '23/02/2022'
-})
